8392f78620db3d443608c00c5e726b0c1a38accb
215410006 <145089293+215410006@users.noreply.github.com>
2023-09-29 06:44:28 +0700

    first commit

--- app/src/main/java/id/utdi/sicerdas/MainActivity.kt
+++ app/src/main/java/id/utdi/sicerdas/MainActivity.kt
@@ -24,6 +24,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            //mengganti pesan nama pada row dan coloumn
             MessageCard(Message("215410006", "Ahmad Riky Dzulfikar"))
         }
     }
@@ -33,24 +34,27 @@ data class Message(val author: String, val body: String)
 
 @Composable
 fun MessageCard(msg: Message) {
-    // Add padding around our message
+    // memberikan row pada aplikasi yang dijalankan
     Row(modifier = Modifier.padding(all = 8.dp)) {
+        //mengatur untuk menampilkan image yang dijalankan pada aplikasi
         Image(
             painter = painterResource(R.drawable.profile_p),
             contentDescription = "Contact profile picture",
             modifier = Modifier
-                // Set image size to 40 dp
+                // mengatur untuk besarnya ukuran image yang ditampilkan
                 .size(40.dp)
-                // Clip image to be shaped as a circle
+                // bentuk image yang ditampilkan
                 .clip(CircleShape)
         )
 
-        // Add a horizontal space between the image and the column
+        // memberikan jarak space pada coloumn
         Spacer(modifier = Modifier.width(8.dp))
 
+        //memeberikan penganturan kolom pada aplikasi yang dibuat
         Column {
             Text(text = msg.author)
-            // Add a vertical space between the author and message texts
+
+            // mwnambahkan spasi vertikal antara penulis dan teks pesan
             Spacer(modifier = Modifier.height(4.dp))
             Text(text = msg.body)
         }
