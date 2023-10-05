19e3c78ee8a76e6ff6c188f01b852cd99144ea21
215410006 <145089293+215410006@users.noreply.github.com>
2023-10-05 22:04:08 +0700

    first commit

--- app/src/main/java/id/utdi/sicerdas/MainActivity.kt
+++ app/src/main/java/id/utdi/sicerdas/MainActivity.kt
@@ -1,30 +1,31 @@
 package id.utdi.sicerdas
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Row
-import androidx.compose.ui.res.painterResource
-import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.platform.LocalContext
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            //mengganti pesan nama pada row dan coloumn
+            // Mengganti pesan nama pada row dan column
             MessageCard(Message("215410006", "Ahmad Riky Dzulfikar"))
         }
     }
@@ -34,29 +35,48 @@ data class Message(val author: String, val body: String)
 
 @Composable
 fun MessageCard(msg: Message) {
-    // memberikan row pada aplikasi yang dijalankan
+    val context = LocalContext.current // Mengakses LocalContext
+
+    // Memberikan row pada aplikasi yang dijalankan
     Row(modifier = Modifier.padding(all = 8.dp)) {
-        //mengatur untuk menampilkan image yang dijalankan pada aplikasi
+        // Mengatur untuk menampilkan image yang dijalankan pada aplikasi
         Image(
             painter = painterResource(R.drawable.profile_p),
             contentDescription = "Contact profile picture",
             modifier = Modifier
-                // mengatur untuk besarnya ukuran image yang ditampilkan
+                // Mengatur untuk besarnya ukuran image yang ditampilkan
                 .size(40.dp)
-                // bentuk image yang ditampilkan
+                // Bentuk image yang ditampilkan
                 .clip(CircleShape)
         )
 
-        // memberikan jarak space pada coloumn
+        // Memberikan jarak space pada column
         Spacer(modifier = Modifier.width(8.dp))
 
-        //memeberikan penganturan kolom pada aplikasi yang dibuat
+        // Memeberikan pengaturan column pada aplikasi yang dibuat
         Column {
             Text(text = msg.author)
 
-            // mwnambahkan spasi vertikal antara penulis dan teks pesan
+            // Menambahkan spasi vertikal antara penulis dan teks pesan
             Spacer(modifier = Modifier.height(4.dp))
             Text(text = msg.body)
+
+            // Tambahkan Button
+            Button(
+                onClick = {
+                    // Kode aksi atau perilaku saat Button diklik
+                    Toast.makeText(
+                        context,
+                        "Tombol di Klik",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(48.dp)
+            ) {
+                Text(text = "Klik disini")
+            }
         }
     }
 }
